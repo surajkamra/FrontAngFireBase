d3c8c88ba2efad9fa2735abf30b84c33dfc132bc
Suraj <surajkumarkamra@gmail.com>
2019-09-12 12:46:03 +0530

    structure

---
+++ src/app/models/user_model.ts
@@ -0,0 +1,14 @@
+export class JbUser {
+    constructor() {
+        this.firstName = '';
+        this.lastName = '';
+        this.email = '';
+        this.password = '';
+        this.confirmPassword = '';
+    }
+    firstName:string
+    lastName:string
+    email:string
+    password:string
+    confirmPassword:string
+}
